82b0fe148bfbf3107e69402ebb75bc49c5de09e2
Jishu Ghosh <ghoshjishu89@gmail.com>
2026-05-12 00:19:40 +0530

    Title case

---
+++ Assignment II/9_convert_senten_cto_title_casse.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class TitleCase
+{
+    void convert()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter sentence:");
+        String str = sc.nextLine();
+
+        String words[] = str.split(" ");
+
+        String result = "";
+
+        for(int i = 0; i < words.length; i++)
+        {
+            String word = words[i];
+
+            char first = Character.toUpperCase(word.charAt(0));
+
+            String rest = word.substring(1);
+
+            result = result + first + rest + " ";
+        }
+
+        System.out.println("Title Case:");
+        System.out.println(result);
+    }
+}
+
+class CovetTitle
+{
+    public static void main(String args[])
+    {
+        TitleCase obj = new TitleCase();
+
+        obj.convert();
+    }
+}
